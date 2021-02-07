ddc079e3f27015eec37dc71d881a7ed1e85d0470
mxncdee <72338626+mxncdee@users.noreply.github.com>
2021-02-07 17:20:31 +0100

    ok

--- README.md
+++
@@ -1,6 +0,0 @@
-#
-
-## Project Setup
-Android App Headlines. Navigazione in differenti categorie. [News API](https://newsapi.org/)
-
-
--- app/src/main/java/com/univaq/mwt/headlines/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.univaq.mwt.headlines.util;
-
-public abstract class Constants {
-    public static final String DATABASE_NAME = "headlines-db";
-}
